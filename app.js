529b3296112f84f4ebf3ee2db0a1b0d48963ef74
Adarsh <59524541+adars-h@users.noreply.github.com>
2025-10-23 13:32:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,31 @@ const login =  require("./routes/login.js");
 const checkAllowance = require("./middleware/Auth/checkAllowance.js");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const { connectDB } = require("./api/index.js")
 // Initializes the express application.
 const app = express();
 app.set("view engine","ejs");
+let isConnected = false; 
+async function connectDB() {
+  try {
+    const db = await mongoose.connect(process.env.CONN, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 45000,
+    });
+    isConnected = db.connections[0].readyState;
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err);
+  }
+}
+app.use((req,res,next)=>{
+	if(!isConnected) {
+		connectDB():
+	}
+	next();
+});
 // configuring middleware functions.
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + "/public"));
