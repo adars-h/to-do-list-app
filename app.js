25e57352ffeb35a7154c096da86c013d19c9d55e
Adarsh <59524541+adars-h@users.noreply.github.com>
2025-10-23 12:03:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 require("dotenv").config();
 const router = require("./routes/route.js");
